1c291edc56088ca1e2013994a6eda4e95b046086
okwonye@gmail.com <okwonye@gmail.com>
2024-04-08 21:33:05 +0900

    - 맞춰보기 button에 strike, ball 판별 함수를 핸들러로 구현

--- bulls-and-cows/src/App.js
+++ bulls-and-cows/src/App.js
@@ -4,18 +4,48 @@ import generateRandomNumber from './random';
 
 function App() {
   const [randomNumber, setRandomNumber] = useState(generateRandomNumber());
+  const [answer, setAnswer] = useState('');
 
   useEffect(() => {
     
   }, [randomNumber])
+
+  const handleAnswerChanged = e => {
+    setAnswer(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    // strike, ball, right
+    const answers = answer.split('').map(item => Number(item));
+    const { strike, ball } = randomNumber.reduce((prev, cur, idx) => {
+      // strike
+      if(answer[idx] === cur) {
+        return {
+          ...prev,
+          strike: prev.strike + 1
+        }
+      }
+
+      // ball
+      if(answer.includes(cur)) {
+        return {
+          ...prev,
+          strike: prev.strike + 1
+        }
+      }
+    }, {
+      strike: 0,
+      ball: 0
+    })
+  }
 
   return (
     <div className="App">
       <h1>숫자 야구 게임</h1>
       <header className="header">{randomNumber}</header>
       <section>
-        <input/>
-        <button>맞춰보기</button>
+        <input type="text" value={answer} onChange={handleAnswerChanged}/>
+        <button onClick={handleSubmit}>맞춰보기</button>
       </section>
       <h2>기록</h2>
       <ol>
