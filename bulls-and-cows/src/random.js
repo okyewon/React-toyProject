c85ae123c0f57f5e12fdfd7ef7eb89adb15ddfa6
okwonye@gmail.com <okwonye@gmail.com>
2024-04-08 21:20:57 +0900

    - 숫자 야구 게임 생성 - JSX로 마크업 작성 및 generateRandomNumber 함수 구현 - useState와 useEffect로 random.js의 함수를 이용해 랜덤 숫자 업데이트

---
+++ bulls-and-cows/src/random.js
@@ -0,0 +1,15 @@
+export default function generateRandomNumber() {
+    // 1 ~ 9까지 숫자 사용
+    const condidates = [1,2,3,4,5,6,7,8,9];
+
+    // 랜덤하게 섞어서 4자리 숫자만 이용할 예정
+    const pickedNumbers = shuffle(condidates).splice(0, 4);
+
+    return pickedNumbers;
+}
+
+function shuffle(array) {
+    return array.sort(() => {
+        return Math.random() - 0.5 // 50% 확률로 음수와 양수를 반환
+    })
+}
