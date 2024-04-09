6c7d2a45a767c2dd82805c1db313626250fd2382
okwonye@gmail.com <okwonye@gmail.com>
2024-04-09 21:22:00 +0900

    - PageHeader 마크업 완료 - Emotion 의 styled로 컴포넌트 스타일링

--- poke-dex/src/Common/PageHeader.tsx
+++ poke-dex/src/Common/PageHeader.tsx
@@ -1,9 +1,37 @@
+import styled from "@emotion/styled";
+
 const PageHeader = () => {
     return (
-        <div>
-            페이지 헤더
-        </div>
+        <Header>
+            <Title>Pokémon</Title>
+            <Select>
+                <option value="Official">Official</option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+            </Select>
+        </Header>
     )
 }
+
+const Header = styled.nav`
+  display: flex;
+  margin-botton: 16px;
+  padding: 16px 32px;
+  border-bottom: 1px solid #c0c0c0;
+`
+
+const Title = styled.h1`
+  margin: 0;
+  font-size: 32px;
+  color: #ffca06;
+  text-shadow: 1px 0 #216fc1, 0 1px #216fc1, -2px 0 #216fc1, 0 -1px #216fc1;
+`
+
+const Select = styled.select`
+    display: flex;
+    margin-left: auto;
+    padding: 8px 12px;
+    border-radius: 8px;
+`
 
 export default PageHeader;
