e27d81655b85eae78583ebede0b54c0696eff746
okwonye@gmail.com <okwonye@gmail.com>
2024-04-10 20:51:19 +0900

    - Network 탭에서 확인시, 아래로 스크롤 후 위로 다시 스크롤 할 때 request를 다시 요청 및 response를 받아오고 있던 것 확인 - thunk 함수에 condition 옵션을 추가해 이미 호출된 data는 다시 불러오지 않도록 구현

--- poke-dex/src/Store/pokemonDetailSlice.ts
+++ poke-dex/src/Store/pokemonDetailSlice.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { act } from 'react-dom/test-utils'
+import { RootState } from '.'
 import { fetchPokemonDetailAPI, PokemonDetailType, PokemonListResponseType } from '../Service/pokemonService'
 
 export const fetchPokemonDetail = createAsyncThunk(
@@ -7,7 +7,15 @@ export const fetchPokemonDetail = createAsyncThunk(
   async (name: string) => {
     const response = await fetchPokemonDetailAPI(name)
     return response
-  },
+  }, {
+    condition: (name, { getState }) => {
+      const { pokemonDetail } = getState() as RootState
+      const pokemon = pokemonDetail.pokemonDetails[name]
+
+      // condition의 return 값이 false이면 2번째 인자의 request를 보내지 X
+      return !pokemon;
+    }
+  }
 )
 
 interface PokemonsDetailState {
